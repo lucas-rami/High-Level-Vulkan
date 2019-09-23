5ae44b97de4d1c6d2b4cdcf2e6dfc29c03c94fec
Lucas Ramirez <lucas.ramirez@epfl.ch>
2019-09-23 22:51:18 +0200

    Documented and re-formatted Device code. Updated clang format.

--- include/device.hpp
+++ include/device.hpp
@@ -3,21 +3,73 @@
 
 #include "hl_vulkan.hpp"
 
+#define NO_SUITABLE_MEMORY_TYPE 255
+
 namespace HLVulkan {
 
-    struct Device {
-        const VkPhysicalDevice physical;
-        const VkDevice logical;
+  /**
+   * @brief Packages a VkPhysicalDevice together with a VkDevice.
+   */
+  struct Device {
+    const VkPhysicalDevice physical;
+    const VkDevice logical;
 
-        Device(VkPhysicalDevice physicalDevice, VkDevice device);
-        Device(const Device &device);
+    /**
+     * @brief Creates a Device instance from a VkPhysicalDevice and a VkDevice.
+     * The logical device must have been created from the physical one.
+     *
+     * @param physicalDevice A physical device.
+     * @param device A logical device.
+     */
+    Device(VkPhysicalDevice physicalDevice, VkDevice device);
 
-        std::optional<uint32_t> findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) const;
+    /**
+     * @brief Straightforward copy constructor (shallow copies all fields).
+     *
+     * @param device The device to copy.
+     */
+    Device(const Device &device);
 
-        VkFormat findSupportedFormat(const std::vector<VkFormat> &candidates, VkImageTiling tiling, VkFormatFeatureFlags features) const;
+    /**
+     * @brief Finds a suitable memory type on the device given a bitmask of
+     * supported memory types and a set of desired properties.
+     *
+     * @param typeFilter A bitmask of supported memory types on the device. Must
+     * be the memoryTypeBits field of a VkMemoryRequirements struct.
+     * @param properties A set of desired properties.
+     *
+     * @return A suitable memory type of minimum index if it exists,
+     * NO_SUITABLE_MEMORY_TYPE otherwise.
+     */
+    uint32_t findMemoryType(uint32_t typeFilter,
+                            VkMemoryPropertyFlags properties) const;
 
-        VkFormat findDepthFormat() const;
-    };
+    /**
+     * @brief Finds a supported format on the device among an ordered list of
+     * candidates.
+     *
+     * @param candidates An ordered list of candidate formats.
+     * @param tiling The tiling to use along the format. One of
+     * VK_IMAGE_TILING_LINEAR or VK_IMAGE_TILING_OPTIMAL.
+     * @param features A set of desired features for the format.
+     *
+     * @return The first format among the candidates which supports the desired
+     * features for the requested tiling mode. VK_FORMAT_UNDEFINED if no format
+     * is supported.
+     */
+    VkFormat findSupportedFormat(const std::vector<VkFormat> &candidates,
+                                 VkImageTiling tiling,
+                                 VkFormatFeatureFlags features) const;
+
+    /**
+     * @brief Finds a supported depth-stencil format on the device. Simply calls
+     * findSupportedFormat() with appropriate arguments for a depth-stencil
+     * format.
+     *
+     * @return See findSupportedFormat().
+     */
+    VkFormat findDepthFormat() const;
+  };
 
 } // namespace HLVulkan
 
--- src/device.cpp
+++ src/device.cpp
@@ -2,47 +2,60 @@
 
 namespace HLVulkan {
 
-    Device::Device(VkPhysicalDevice physicalDevice, VkDevice device) : physical(physicalDevice), logical(device) {}
-    Device::Device(const Device &device) : physical(device.physical), logical(device.logical) {}
+  Device::Device(VkPhysicalDevice physicalDevice, VkDevice device)
+      : physical(physicalDevice), logical(device) {}
+  Device::Device(const Device &device)
+      : physical(device.physical), logical(device.logical) {}
 
-    std::optional<uint32_t> Device::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) const {
+  uint32_t Device::findMemoryType(uint32_t typeFilter,
+                                  VkMemoryPropertyFlags properties) const {
 
-        // Query physical device for available types of memory
-        VkPhysicalDeviceMemoryProperties memProperties;
-        vkGetPhysicalDeviceMemoryProperties(physical, &memProperties);
+    // Query physical device for available types of memory
+    VkPhysicalDeviceMemoryProperties memProperties;
+    vkGetPhysicalDeviceMemoryProperties(physical, &memProperties);
 
-        // Check each type of memory
-        for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
-            if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
-                return i;
-            }
-        }
-
-        // No suitable type found
-        return {};
+    // Check each type of memory
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+      if ((typeFilter & (1 << i)) &&
+          (memProperties.memoryTypes[i].propertyFlags & properties) ==
+              properties) {
+        return i;
+      }
     }
 
-    VkFormat Device::findSupportedFormat(const std::vector<VkFormat> &candidates, VkImageTiling tiling, VkFormatFeatureFlags features) const {
+    // No suitable type found
+    return NO_SUITABLE_MEMORY_TYPE;
+  }
 
-        // Check each candidate format for the necessary features
-        for (VkFormat format : candidates) {
-            VkFormatProperties props;
-            vkGetPhysicalDeviceFormatProperties(physical, format, &props);
+  VkFormat Device::findSupportedFormat(const std::vector<VkFormat> &candidates,
+                                       VkImageTiling tiling,
+                                       VkFormatFeatureFlags features) const {
 
-            if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features) {
-                return format;
-            } else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features) {
-                return format;
-            }
-        }
+    // Check each candidate format for the necessary features
+    for (const auto format : candidates) {
+      VkFormatProperties props;
+      vkGetPhysicalDeviceFormatProperties(physical, format, &props);
 
-        ASSERT_MSG(false, "failed to find supported format");
-        return VK_FORMAT_UNDEFINED;
+      if (tiling == VK_IMAGE_TILING_LINEAR &&
+          (props.linearTilingFeatures & features) == features) {
+        return format;
+      } else if (tiling == VK_IMAGE_TILING_OPTIMAL &&
+                 (props.optimalTilingFeatures & features) == features) {
+        return format;
+      }
     }
 
-    VkFormat Device::findDepthFormat() const {
-        return findSupportedFormat({VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT}, VK_IMAGE_TILING_OPTIMAL,
-                                   VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
-    }
+    // No candidate format was found with the necessary features
+    ASSERT_MSG(false, "failed to find supported format");
+    return VK_FORMAT_UNDEFINED;
+  }
+
+  VkFormat Device::findDepthFormat() const {
+    return findSupportedFormat({VK_FORMAT_D32_SFLOAT,
+                                VK_FORMAT_D32_SFLOAT_S8_UINT,
+                                VK_FORMAT_D24_UNORM_S8_UINT},
+                               VK_IMAGE_TILING_OPTIMAL,
+                               VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
+  }
 
 } // namespace HLVulkan
