9984de643ac44cd99736a2483ae5771fd737def4
Lucas Ramirez <lucas.ramirez@epfl.ch>
2019-12-29 23:16:15 +0100

    Moved pipeline creation code to Pipeline. Documented Pipeline.
    Removed PipelineFactory.

--- include/pipeline.hpp
+++ include/pipeline.hpp
@@ -1,33 +1,66 @@
 #ifndef __HL_VULKAN_PIPELINE_HPP__
 #define __HL_VULKAN_PIPELINE_HPP__
 
-#include "hl_vulkan.hpp"
+#include "device.hpp"
+#include "pipeline_spec.hpp"
+#include "shader.hpp"
+#include "vertex_spec.hpp"
 
 namespace HLVulkan {
 
+  /**
+   * @brief Packages a VkPipeline and VkPipelineLayout.
+   */
   class Pipeline {
   public:
-    Pipeline(VkDevice device);
+    /**
+     * @brief Creates a pipeline following some vertex and pipeline
+     * specification.
+     *
+     * @param[in] device The device to allocate the pipeline from.
+     * @param[in] vertSpec A vertex specification.
+     * @param[in] pipeSpec A pipeline specification.
+     * @param[in] shaders The set of shaders involved in the pipeline.
+     * @param[in] renderPass The render pass associated to the pipeline.
+     *
+     * @throw std::runtime_error If pipeline creation fails for some reason.
+     */
+    Pipeline(const Device &device, const VertexSpec &vertSpec,
+             const PipelineSpec &pipeSpec, const std::vector<Shader> &shaders,
+             VkRenderPass renderPass);
 
+    /**
+     * @brief Deleted copy-constructor.
+     */
     Pipeline(const Pipeline &other) = delete;
 
+    /**
+     * @brief Deleted copy-assignment operator.
+     */
     Pipeline &operator=(const Pipeline &other) = delete;
 
+    /**
+     * @brief Move-constructor.
+     */
     Pipeline(Pipeline &&other);
 
+    /**
+     * @brief Move-assignment operator.
+     */
     Pipeline &operator=(Pipeline &&other);
 
-    VkResult create(const VkPipelineLayoutCreateInfo &layoutInfo,
-                    VkGraphicsPipelineCreateInfo &pipelineInfo);
-
-    VkDevice getDevice() const;
-
+    /**
+     * @brief Destructor.
+     */
     virtual ~Pipeline();
 
   private:
-    VkDevice device = VK_NULL_HANDLE;
-    VkPipeline handle = VK_NULL_HANDLE;
-    VkPipelineLayout layout = VK_NULL_HANDLE;
+    // The device to allocate the pipeline from
+    VkDevice device;
+    // The pipeline handle
+    VkPipeline pipeline{VK_NULL_HANDLE};
+    // The pipeline layout handle
+    VkPipelineLayout layout{VK_NULL_HANDLE};
   };
 
 } // namespace HLVulkan
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -2,53 +2,147 @@
 
 namespace HLVulkan {
 
-  Pipeline::Pipeline(VkDevice device) : device(device) {}
+  Pipeline::Pipeline(const Device &device, const VertexSpec &vertSpec,
+                     const PipelineSpec &pipeSpec,
+                     const std::vector<Shader> &shaders,
+                     VkRenderPass renderPass)
+      : device(*device) {
+
+    // ***** Create pipeline layout *****
+
+    // Descriptor set layouts
+    std::vector<VkDescriptorSetLayout> descSetLayouts;
+    pipeSpec.getDescriptorSetLayouts(descSetLayouts);
+
+    // Pipeline layout (depends on descSetLayouts)
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount =
+        static_cast<uint32_t>(descSetLayouts.size());
+    pipelineLayoutInfo.pSetLayouts = descSetLayouts.data();
+
+    VK_THROW(vkCreatePipelineLayout(this->device, &pipelineLayoutInfo, nullptr,
+                                    &layout),
+             "failed to create layout");
+
+    // ***** Create pipeline *****
+
+    // Vertex specification
+    std::vector<VkVertexInputAttributeDescription> attributes;
+    vertSpec.getAttributeDescriptions(attributes);
+    std::vector<VkVertexInputBindingDescription> bindings{};
+    vertSpec.getBindingDescriptions(bindings);
+
+    // Vertex input
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+    vertexInputInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexBindingDescriptionCount =
+        static_cast<uint32_t>(bindings.size());
+    vertexInputInfo.pVertexBindingDescriptions = bindings.data();
+    vertexInputInfo.vertexAttributeDescriptionCount =
+        static_cast<uint32_t>(attributes.size());
+    vertexInputInfo.pVertexAttributeDescriptions = attributes.data();
+
+    // Input assembly
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly;
+    pipeSpec.getInputAssembly(inputAssembly);
+
+    // Viewports
+    std::vector<VkViewport> viewports;
+    pipeSpec.getViewports(viewports);
+
+    // Scissors
+    std::vector<VkRect2D> scissors;
+    pipeSpec.getScissors(scissors);
+
+    // Viewport state
+    VkPipelineViewportStateCreateInfo viewportState = {};
+    viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportState.viewportCount = static_cast<uint32_t>(viewports.size());
+    viewportState.pViewports = viewports.data();
+    viewportState.scissorCount = static_cast<uint32_t>(scissors.size());
+    viewportState.pScissors = scissors.data();
+
+    // Rasterizer
+    VkPipelineRasterizationStateCreateInfo rasterizer;
+    pipeSpec.getRasterizer(rasterizer);
+
+    // Multisampling
+    VkPipelineMultisampleStateCreateInfo multisampling;
+    pipeSpec.getMultisampling(multisampling);
+
+    // Color blending
+    std::vector<VkPipelineColorBlendAttachmentState> colorBlends;
+    pipeSpec.getColorBlending(colorBlends);
+
+    // Color blend state (depends on colorBlends)
+    VkPipelineColorBlendStateCreateInfo colorBlending = {};
+    colorBlending.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colorBlending.logicOpEnable = VK_FALSE;
+    colorBlending.attachmentCount = static_cast<uint32_t>(colorBlends.size());
+    colorBlending.pAttachments = colorBlends.data();
+
+    // Depth/Stencil
+    VkPipelineDepthStencilStateCreateInfo depthStencil;
+    pipeSpec.getDepthStencil(depthStencil);
+
+    // Shader stages
+    std::vector<VkPipelineShaderStageCreateInfo> shaderStages{shaders.size()};
+    for (const auto &shader : shaders) {
+      shaderStages.push_back(shader.getInfo());
+    }
+
+    // Final structure (depends on all of the above + render pass)
+    VkGraphicsPipelineCreateInfo pipelineInfo = {};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = static_cast<uint32_t>(shaderStages.size());
+    pipelineInfo.pStages = shaderStages.data();
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterizer;
+    pipelineInfo.pMultisampleState = &multisampling;
+    pipelineInfo.pDepthStencilState = &depthStencil;
+    pipelineInfo.pColorBlendState = &colorBlending;
+    pipelineInfo.pDynamicState = nullptr;
+    pipelineInfo.layout = layout;
+    pipelineInfo.renderPass = renderPass;
+    pipelineInfo.subpass = 0;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+    pipelineInfo.basePipelineIndex = -1;
+
+    // Create pipeline
+    VkResult ret;
+    if ((ret = vkCreateGraphicsPipelines(this->device, VK_NULL_HANDLE, 1,
+                                         &pipelineInfo, nullptr, &pipeline)) !=
+        VK_SUCCESS) {
+      vkDestroyPipelineLayout(this->device, layout, nullptr);
+      ASSERT_THROW(false, "failed to create pipeline")
+    }
+  }
 
   Pipeline::Pipeline(Pipeline &&other)
-      : device(other.device), handle(other.handle), layout(other.layout) {
-    other.handle = VK_NULL_HANDLE;
+      : device(other.device), pipeline(other.pipeline), layout(other.layout) {
+    other.pipeline = VK_NULL_HANDLE;
   }
 
   Pipeline &Pipeline::operator=(Pipeline &&other) {
     // Self-assignment detection
     if (&other != this) {
       device = other.device;
-      handle = other.handle;
+      pipeline = other.pipeline;
       layout = other.layout;
 
-      other.handle = VK_NULL_HANDLE;
+      other.pipeline = VK_NULL_HANDLE;
     }
     return *this;
   };
 
-  VkResult Pipeline::create(const VkPipelineLayoutCreateInfo &layoutInfo,
-                            VkGraphicsPipelineCreateInfo &pipelineInfo) {
-
-    ASSERT_FAIL(handle == VK_NULL_HANDLE, "pipeline already created");
-
-    // Create layout
-    VK_RET(vkCreatePipelineLayout(device, &layoutInfo, nullptr, &layout));
-
-    // Update layout in pipeline creation structure
-    pipelineInfo.layout = layout;
-
-    // Create pipeline
-    VkResult ret;
-    if ((ret = vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1,
-                                         &pipelineInfo, nullptr, &handle)) !=
-        VK_SUCCESS) {
-      vkDestroyPipelineLayout(device, layout, nullptr);
-      return ret;
-    }
-
-    return VK_SUCCESS;
-  }
-
-  VkDevice Pipeline::getDevice() const { return device; }
-
   Pipeline::~Pipeline() {
-    if (handle != VK_NULL_HANDLE) {
-      vkDestroyPipeline(device, handle, nullptr);
+    if (pipeline != VK_NULL_HANDLE) {
+      vkDestroyPipeline(device, pipeline, nullptr);
       vkDestroyPipelineLayout(device, layout, nullptr);
     }
   }
